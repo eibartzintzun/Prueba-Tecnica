c55f8026935cffcd451c1e7234177bdc13c2ef98
eibartzintzun <eibar_tzz@outlook.com>
2026-02-20 16:10:28 -0600

    Se agregan los archivos de tests

---
+++ PruebaTecnica/PruebaTecnicaTests/PruebaTecnicaTests.swift
@@ -0,0 +1,16 @@
+//
+//  PruebaTecnicaTests.swift
+//  PruebaTecnicaTests
+//
+//  Created by Eibar Tzintzun on 20/02/26.
+//
+
+import Testing
+
+struct PruebaTecnicaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+    
+}
